4394ac8492d512a10456e2a916fbc4db58dde43a
Steven Curtis <stevenpcurtis@hotmail.com>
2019-08-03 13:29:55 +0800

    Initial Commit

---
+++ SimpleMVVMNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleMVVMNetwork
+//
+//  Created by Steven Curtis on 03/08/2019.
+//  Copyright © 2019 Steven Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
